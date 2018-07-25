6658c203d8862fa33d9b615f188d45e8690c4470
pedro <pedrosanchez@droiders.com>
2018-07-25 09:24:36 +0200

    add readme and more

--- app/src/main/java/com/pedro/mvvmexample/repository/RepositoryTask.kt
+++ app/src/main/java/com/pedro/mvvmexample/repository/RepositoryTask.kt
@@ -4,4 +4,5 @@ import android.content.Context
 import com.pedro.mvvmexample.task.LoginTask
 import com.pedro.mvvmexample.task.NavigatorTask
 
-data class RepositoryTask(private val context: Context, val loginTask: LoginTask = LoginTask(), val navigatorTask: NavigatorTask = NavigatorTask(context))
+data class RepositoryTask(private val context: Context, val loginTask: LoginTask = LoginTask(),
+                          val navigatorTask: NavigatorTask = NavigatorTask(context))
